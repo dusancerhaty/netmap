004b3c046ededb4e2d5d4803c714774e098b0be5
Dusan Cerhaty <cerhaty@eset.sk>
2019-11-04 07:37:01 +0100

    Add build helper script

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function action() {
+	local verbose=false
+	local output=""
+
+	if [ "$1" = "-v" ]; then
+		verbose=true
+		shift
+	fi
+	echo -n "$@ ... "
+	output="$($@)"
+	if [ $? -ne 0 ]; then
+		echo "FAIL"
+		echo "$output"
+		exit 1
+	fi
+	echo "OK"
+	if [ "$verbose" = "true" ] && ! [ -z "$output" ] ; then
+		echo "$output"
+	fi
+}
+
+action $1 make
+
+action $1 make install
+
+action $1 modprobe -r igbnetmap
+
+action $1 modprobe igbnetmap
+
+action $1 ~/workspace/prepareIfaces.sh eth1,eth2,eth3,eth4 1 igb false 256
